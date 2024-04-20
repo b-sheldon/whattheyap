234139bba4f0d72f33b9a39a57eb0be46b395bb8
Ben Sheldon <bsheldon02@gmail.com>
2024-04-20 18:01:32 -0400

    Move everything to app

--- src/index.jsx
+++ src/index.jsx
@@ -1,50 +1,6 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
-import 'tailwindcss/tailwind.css';
-import './style.scss';
-
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Dashboard from './pages/Dashboard';
-import Create from './pages/Create';
-import TextToSpeech from './pages/TextToSpeech';
-
-const Welcome = (props) => {
-  const navigate = useNavigate();
-
-  return (
-    <div>
-      <h1>Welcome to What The Yap?</h1>
-      <button type="button" onClick={() => navigate('/text-to-speech')}>speech</button>
-      <button type="button" onClick={() => navigate('/signin')}>Sign In</button>
-      <button type="button" onClick={() => navigate('/signup')}>Sign Up</button>
-      <button type="button" onClick={() => navigate('/create')}>Create</button>
-    </div>
-  );
-};
-
-const FallBack = (props) => {
-  return <div>URL Not Found</div>;
-};
-
-const App = (props) => {
-  return (
-    <BrowserRouter>
-      <div>
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/text-to-speech" element={<TextToSpeech />} />
-          <Route path="*" element={<FallBack />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
-};
+import { createRoot } from 'react-dom';
+import App from './pages/App';
 
 const root = createRoot(document.getElementById('main'));
 root.render(<App />);
---
+++ src/pages/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import 'tailwindcss/tailwind.css';
+
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import Dashboard from './Dashboard';
+import Create from './Create';
+import TextToSpeech from './TextToSpeech';
+
+const Welcome = (props) => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Welcome to What The Yap?</h1>
+      <button type="button" onClick={() => navigate('/text-to-speech')}>speech</button>
+      <button type="button" onClick={() => navigate('/signin')}>Sign In</button>
+      <button type="button" onClick={() => navigate('/signup')}>Sign Up</button>
+      <button type="button" onClick={() => navigate('/create')}>Create</button>
+    </div>
+  );
+};
+
+const FallBack = (props) => {
+  return <div>URL Not Found</div>;
+};
+
+const App = (props) => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/text-to-speech" element={<TextToSpeech />} />
+          <Route path="*" element={<FallBack />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
