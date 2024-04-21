0803bb4ae55560a037015bfa986491ac32faf9c4
dstarr25 <91504587+dstarr25@users.noreply.github.com>
2024-04-21 01:37:40 -0400

    quiz navigation and flow mostly done

--- src/pages/FlashCardsLearn.jsx
+++ src/pages/FlashCardsLearn.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import useTTS from '../functions/tts';
 import useStore from '../store/zustand';
 import IconVolumeOn from '../icons/IconVolumeOn';
 import IconVolumeOff from '../icons/IconVolumeOff';
+import IconBackArrow from '../icons/IconBackArrow';
 
 const timeSleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const FlashcardsLearn = () => {
+    const navigate = useNavigate();
     const { currentFlashcards, speechMode, setSpeechMode, currentTitle } = useStore();
     const [currIndex, setCurrIndex] = useState(-1);
     const [answer, setAnswer] = useState('');
@@ -78,18 +81,18 @@ const FlashcardsLearn = () => {
             const feedback = correct ? 'Correct!' : `Incorrect. The correct answer was: ${currentFlashcards[currIndex - 1].a}.`;
             setFeedback(feedback);
             await condTTS(feedback);
-            if (!speechMode) await timeSleep(2000);
             setAnswer('');
+            if (!speechMode) await timeSleep(3000);
             setFeedback('');
-            await condTTS(`Next question is: ${currentFlashcards[currIndex].q}`);
+            await condTTS(`${currentFlashcards[currIndex].q}`);
             answerBox.current.focus();   
             condSTT();
         } else if (currIndex >= currentFlashcards.length) {
             const feedback = correct ? 'Correct!' : `Incorrect. The correct answer was: ${currentFlashcards[currIndex - 1].a}.`
             setFeedback(feedback);
             await condTTS(feedback);
-            if (!speechMode) await timeSleep(2000);
-            setFeedback('Quiz complete.');
+            if (!speechMode) await timeSleep(3000);
+            setFeedback('');
             await condTTS('Quiz complete.');
         }
     };
@@ -106,6 +109,35 @@ const FlashcardsLearn = () => {
     
     }
 
+    const questionBoxText = () => {
+        if (!feedback) {
+            if (currIndex < currentFlashcards.length) return currentFlashcards[currIndex].q;
+            return 'Quiz Complete.';
+        }
+        return feedback;
+    }
+
+    const questionBoxColor = () => {
+        if (!feedback) return 'text-black';
+        if (correct) return 'text-green-800';
+        return 'text-red-800';
+    }
+
+    const toggleSpeechMode = async () => {
+        const temp = speechMode;
+        setSpeechMode(!speechMode);
+        if (temp) {
+            console.log('ending speech mode');
+            // handleMute();
+            return;
+        }
+        console.log('starting speech mode');
+        await textToSpeech(currentFlashcards[currIndex].q);
+        sttFromMic();
+        answerBox.current.focus();
+    }
+
+
     useEffect(() => {
         handleNextQuesion();
     }, [currIndex]);
@@ -115,19 +147,26 @@ const FlashcardsLearn = () => {
 
     return (
     <div className="flex-grow flex flex-col items-center p-10 gap-6 text-xl">
-        <div className="w-full flex justify-end items-center gap-4 border-b-2 py-6">
-            <div className='flex flex-col justify-center'>
-                <div className='leading-none'>audio</div>
-                <div className='leading-none'>mode</div>
+        <div className="w-full flex justify-between items-center gap-4 border-b-2 py-6">
+            <button onClick={() => navigate("/dashboard")}><IconBackArrow className="cursor-pointer w-10 h-10 hover:scale-110 hover:-rotate-6 transition" /></button>
+            <div className="flex gap-2">
+                <div className='flex flex-col justify-center'>
+                    <div className='leading-none'>audio</div>
+                    <div className='leading-none'>mode</div>
+                </div>
+                <button className=" relative w-10 h-10" onClick={toggleSpeechMode}>
+                    <IconVolumeOn className="absolute top-0 left-0 transition-all text-green-800" style={{ opacity: speechMode ? 1 : 0 }} />
+                    <IconVolumeOff className="absolute top-0 left-0 transition-all text-red-800" style={{ opacity: !speechMode ? 1 : 0 }} />
+                </button>
             </div>
-            <button className=" relative w-10 h-10" onClick={() => setSpeechMode(!speechMode)}>
-                <IconVolumeOn className="absolute top-0 left-0 transition-all text-green-800" style={{ opacity: speechMode ? 1 : 0 }} />
-                <IconVolumeOff className="absolute top-0 left-0 transition-all text-red-800" style={{ opacity: !speechMode ? 1 : 0 }} />
-            </button>
         </div>
         <div className="self-start font-bold text-2xl">{currentTitle}</div>
         <div className="w-2/3 flex flex-col items-center gap-6">
-            <div className="w-full bg-purplelight flex justify-center items-center border-2 border-purplelight rounded-xl focus:outline-purpledark shadow-md p-4">{ currIndex < currentFlashcards.length ? currentFlashcards[currIndex].q : 'Quiz Complete.'}</div>
+            <div 
+                className={`w-full bg-purplelight flex justify-center items-center border-2 border-purplelight rounded-xl focus:outline-purpledark shadow-md p-4 ${questionBoxColor()}`}
+            >
+                {questionBoxText()}
+            </div>
             <form onSubmit={submitAnswer} className='w-full flex flex-col items-center gap-6'>
                 <textarea
                     ref={answerBox}
